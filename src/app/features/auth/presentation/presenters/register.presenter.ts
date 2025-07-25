46e75e59cb82803006c82d603e1f7d65324c109f
Jeanpierre Ponte Solis <jeanpierre.ponte@auna.org>
2025-07-25 10:26:10 -0500

    feat: new changes and fix design

--- src/app/features/auth/presentation/presenters/register.presenter.ts
+++ src/app/features/auth/presentation/presenters/register.presenter.ts
@@ -1,11 +1,19 @@
 import { Injectable } from "@angular/core";
+import { LoginUseCase } from "@auth/application/use-cases/login.use-case";
 import { RegisterUseCase } from "@auth/application/use-cases/register.use-case";
-import { RegisterRequest } from "@auth/domain/repositories/user-repository.interface";
+import { LoginRequest, RegisterRequest } from "@auth/domain/repositories/user-repository.interface";
 import { RegisterView } from "@auth/presentation/views/register.view.interface";
+import { NotificationService } from "@core/services/notification.service";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable()
 export class RegisterPresenter {
-  constructor(private registerUseCase: RegisterUseCase) {}
+  constructor(
+    private registerUseCase: RegisterUseCase,
+    private loginUseCase: LoginUseCase,
+    private notificationService: NotificationService,
+    private translateService: TranslateService
+  ) {}
 
   async register(view: RegisterView, userData: RegisterRequest): Promise<void> {
     view.showLoading();
@@ -13,7 +21,17 @@ export class RegisterPresenter {
 
     try {
       await this.registerUseCase.execute(userData).toPromise();
+
+      const loginCredentials: LoginRequest = {
+        email: userData.email,
+      };
+
+      await this.loginUseCase.execute(loginCredentials).toPromise();
+
       view.hideLoading();
+
+      const successMessage = this.translateService.instant("AUTH.ACCOUNT_CREATED_AND_LOGGED");
+      this.notificationService.showSuccess(successMessage);
       view.navigateToKanban();
     } catch (error: any) {
       view.hideLoading();
